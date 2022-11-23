d09f092eef33364310a1ac54dc148d0ce7e421a2
chrisbrugger <cbchristopherbrugger@gmail.com>
2022-11-23 16:05:48 +0100

    First commit

---
+++ IdeaProjects/ccftp/src/test/kotlin/com/cbhk/ccftp/CcftpApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cbhk.ccftp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CcftpApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
